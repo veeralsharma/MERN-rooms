157e755cbddab92c146e1c3de645bba9680070f2
veeralsharma <veeralsharma0001@gmail.com>
2020-10-07 00:48:19 +0530

    added context api and deployment

--- whatsapp-frontend/src/firebase.js
+++ whatsapp-frontend/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBAm4pLPU2tw3tlIRMhC0xLVQcKd16i0fk",
-    authDomain: "whatsapp-mern-8e5a7.firebaseapp.com",
-    databaseURL: "https://whatsapp-mern-8e5a7.firebaseio.com",
-    projectId: "whatsapp-mern-8e5a7",
-    storageBucket: "whatsapp-mern-8e5a7.appspot.com",
-    messagingSenderId: "9194508907",
-    appId: "1:9194508907:web:51046feed9e59ea210172e",
-    measurementId: "G-HEMCFTPKGF"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT
   };
 
   
