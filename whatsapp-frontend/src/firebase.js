bc5129d2a622792e590500e9855e96fa22331e91
veeralsharma <veeralsharma0001@gmail.com>
2020-10-28 00:21:58 +0530

    env changes

--- whatsapp-frontend/src/firebase.js
+++ whatsapp-frontend/src/firebase.js
@@ -1,15 +1,16 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBAm4pLPU2tw3tlIRMhC0xLVQcKd16i0fk",
-    authDomain: "whatsapp-mern-8e5a7.firebaseapp.com",
-    databaseURL: "https://whatsapp-mern-8e5a7.firebaseio.com",
-    projectId: "whatsapp-mern-8e5a7",
-    storageBucket: "whatsapp-mern-8e5a7.appspot.com",
-    messagingSenderId: "9194508907",
-    appId: "1:9194508907:web:51046feed9e59ea210172e",
-    measurementId: "G-HEMCFTPKGF"
-  };
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
 
   
 const firebaseApp = firebase.initializeApp(firebaseConfig)
